0ff1279f1574a5ce4c920c5942a10ef7381705fc
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-05-14 18:58:26 +0200

    140522 add wallet

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,43 @@ import Link from "next/link";
 import Sidebar from "../components/Sidebar";
 import Rightbar from "../components/Rightbar";
 import MainContent from "./MainContent";
+import { useMoralis } from "react-moralis";
+import { ConnectButton, Icon } from "web3uikit";
 
 export default function Home() {
+  const { isAuthenticated, Moralis } = useMoralis();
   return (
     <>
-      <div className="page">
-        <div className="sideBar">
-          <Sidebar />
-        </div>
+      {isAuthenticated ? (
+        <div className="page">
+          <div className="sideBar">
+            <Sidebar />
+            <div
+              className="logout"
+              onClick={() => {
+                Moralis.User.logOut().then(() => {
+                  window.location.reload();
+                });
+              }}
+            >
+              Logout
+            </div>
+          </div>
 
-        <div className="mainWindow">
-          <MainContent />
-        </div>
+          <div className="mainWindow">
+            <MainContent />
+          </div>
 
-        <div className="rightBar">
-          <Rightbar />
+          <div className="rightBar">
+            <Rightbar />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="loginPage">
+          <Icon fill="#ffffff" size={40} svg="twitter" />
+          <ConnectButton />
+        </div>
+      )}
     </>
   );
 }
